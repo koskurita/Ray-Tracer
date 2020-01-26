c967a5c9e4d4f8b6031ff7fb2dd610a68e803f3a
koskurita <kkuri005@ucr.edu>
2020-01-26 02:10:14 -0800

    Update sphere.cpp

--- sphere.cpp
+++ sphere.cpp
@@ -6,14 +6,13 @@ Hit Sphere::Intersection(const Ray& ray, int part) const
 {
     vec3 endpt_to_center = center - ray.endpoint;
     double time = dot(endpt_to_center, ray.direction);
-    vec3 t = Point(time);
+    vec3 t = ray.Point(time);
     vec3 vec_t = center - t;
     double distance_x = sqrt(pow(radius, 2) - vec_t.magnitude_squared());
     double intersection_time = time - distance_x;
     
     Hit hit;
-    hit.object = new Sphere;
-    hit.object = *this;
+    hit.object = this;
     hit.dist = intersection_time;
 
     return hit;
