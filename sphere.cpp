35521c1f1b2efe290a8d2966513912012937b8cc
koskurita <kkuri005@ucr.edu>
2020-01-26 01:51:41 -0800

    ray tracer
    Updated sphere intersection

--- sphere.cpp
+++ sphere.cpp
@@ -4,8 +4,29 @@
 // Determine if the ray intersects with the sphere
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
-    TODO;
-    return {0,0,0};
+    vec3 endpt_to_center = center - ray.endpoint;
+    double time = dot(endpt_to_center, ray);
+    vec3 t = Point(time);
+    vec3 vec_t = center - t;
+    double distance_x = sqrt(pow(radius, 2) - vec_t.magnitude_squared());
+    double intersection_time = time - distance_x;
+    
+    Hit hit;
+    hit.object = *this;
+    hit.dist = intersection_time;
+
+    return hit;
+    
+    /*
+    vec3 t1p = ro + rd * t1;
+    vec3 sphereToOrigin = normalize(ro - s);
+    vec3 normal = normalize(t1p - s);
+    col = vec3(dot(normal, sphereToOrigin));
+    */
+    
+    
+    
+   // return {0,0,0};
 }
 
 vec3 Sphere::Normal(const vec3& point, int part) const
