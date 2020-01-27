335099352e71f0780df6609f3b8b15b991bc3fdb
koskurita <kkuri005@ucr.edu>
2020-01-27 13:43:51 -0800

    Update sphere.cpp

--- sphere.cpp
+++ sphere.cpp
@@ -16,16 +16,19 @@ Hit Sphere::Intersection(const Ray& ray, int part) const
     double t2 = -b + sqrt(b*b)-c;
     
     if(t1 < 0){
+                std::cout << "hello";
         return {this, t2, 0};
     }
     else if(t2 < 0){
+                std::cout << "hello";
         return {this, t1, 0};
     }
     else if(t1 < t2){
-        std::cout << "hello";
+                std::cout << "hello";
         return {this, t1, 0};
     }
     else if(t2 > t1){
+                std::cout << "hello";
         return {this, t2, 0};
     }
     else{
