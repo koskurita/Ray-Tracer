62625c923baee8bf7cba7b1f75e40f76d59dad8f
koskurita <kkuri005@ucr.edu>
2020-01-26 22:30:23 -0800

    Update render_world.cpp

--- render_world.cpp
+++ render_world.cpp
@@ -70,7 +70,7 @@ vec3 Render_World::Cast_Ray(const Ray& ray,int recursion_depth)
     if(closest_hit.object != nullptr){
         const Object *intersected_obj = closest_hit.object;
         vec3 intersection_pt = ray.Point(closest_hit.dist);
-        vec3 normal = intersected_obj->Normal(intersected_pt, closest_hit.part);
+        vec3 normal = intersected_obj->Normal(intersection_pt, closest_hit.part);
         
         color = intersected_obj->material_shader->Shade_Surface(ray, intersected_pt, normal, recursion_depth);
     }
