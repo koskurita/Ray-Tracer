47f57c065569a2f4b31d2ef4c4ab6bc700df9229
koskurita <kkuri005@ucr.edu>
2020-01-26 23:07:32 -0800

    Update sphere.cpp

--- sphere.cpp
+++ sphere.cpp
@@ -19,7 +19,7 @@ Hit Sphere::Intersection(const Ray& ray, int part) const
 
     return hit;
      */
-    std:: cout << ray;
+    std:: cout << ray.endpoint << " " << ray.direction << std::endl;
     vec3 v = ray.endpoint - this->center;
     vec3 w = ray.direction;
     
