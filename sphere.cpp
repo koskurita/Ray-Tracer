5deb330c05cd9af09fdd8cd056e2403e89de4342
koskurita <kkuri005@ucr.edu>
2020-01-27 15:57:46 -0800

    Update sphere.cpp

--- sphere.cpp
+++ sphere.cpp
@@ -10,34 +10,41 @@ Hit Sphere::Intersection(const Ray& ray, int part) const
     double b = dot(ray.direction*2, L);
     double c = dot(L, L) - radius*radius;
     
-    double discriminant = b*b - 4*c;
+    double discriminant = (b*b) - (4*c);
     
     if(discriminant < 0){
+        std::cout << " test 1";
         return null;
     }
+    
     double t1 = (-b - sqrt(discriminant))/2;
     double t2 = (-b + sqrt(discriminant))/2;
     
     if(t1 < 0){
+        std::cout << " test 2";
     //            std::cout << "hello";
         Hit hit = {this, t2, 0};
         return hit;
     }
     else if(t2 < 0){
+        std::cout << " test 3";
       Hit hit = {this, t1, 0};
         return hit;
     }
     else if(t1 < t2){
+        std::cout << " test 4";
     //            std::cout << "hello";
         Hit hit = {this, t1, 0};
         return hit;
     }
     else if(t2 > t1){
+        std::cout << " test 5";
     //            std::cout << "hello";
         Hit hit = {this, t2, 0};
         return hit;
     }
     else{
+        std::cout << " test 6";
         return null;
     }
 
