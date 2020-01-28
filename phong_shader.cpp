70f9070aedbbb84ca3977153410a933ef91ad0c6
koskurita <kkuri005@ucr.edu>
2020-01-27 17:55:08 -0800

    Update phong_shader.cpp

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -9,6 +9,18 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
     vec3 color;
-    TODO; //determine the color
+    Ray light_ray;
+    
+    for(unsigned int i = 0; i < world.lights.size(); i++){
+        light_ray.direction = intersection_point - world.lights[i].position;
+        light_ray.direction = light_ray.normalized();
+        light_ray.endpoint = world.lights[i]->position;
+        
+        vec3 l = intersection_point - world.lights[i].position;
+        vec3 intensity = world.lights[i].Emitted_Light(light_ray);
+        
+        color = (intensity * color_diffuse * world.lights[i].brightness  * std::max(0, dot(normal.normalized(), l)));
+    }
+    
     return color;
 }
