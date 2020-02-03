a9875236bc85a4943e93a360e2dbce7c46b32b44
koskurita <kkuri005@ucr.edu>
2020-02-03 00:33:51 -0800

    Update phong_shader.cpp

--- phong_shader.cpp
+++ phong_shader.cpp
@@ -8,20 +8,20 @@ vec3 Phong_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
     const vec3& normal,int recursion_depth) const
 {
-    vec3 color;
+    vec3 color = color_ambient * (world.ambient_color * world.ambient_intensity);
     Ray light_ray;
     vec3 light_color;
     
     for(unsigned int i = 0; i < world.lights.size(); ++i) {
-     light_ray.direction = world.lights.at(i)->position - intersection_point;
-     double mag_squared = light_ray.direction.magnitude_squared();
-     light_ray.direction = light_ray.direction.normalized();
-     light_ray.endpoint = world.lights.at(i)->position;
+        light_ray.direction = world.lights.at(i)->position - intersection_point;
+        double mag_squared = light_ray.direction.magnitude_squared();
+        light_ray.direction = light_ray.direction.normalized();
+        light_ray.endpoint = world.lights.at(i)->position;
         
-     light_color = world.lights[i]->Emitted_Light(light_ray.direction);
-     light_color = light_color/mag_squared;
-     double temp = std::max(dot(light_ray.direction, normal) , 0.0);
-     color = (light_color * color_diffuse * temp);
+        light_color = world.lights[i]->Emitted_Light(light_ray.direction);
+        light_color = light_color/mag_squared;
+        double temp = std::max(dot(light_ray.direction, normal) , 0.0);
+        color = (light_color * color_diffuse * temp);
     }
     
     return color;
